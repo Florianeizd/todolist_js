52e271f1786ce8c35e738478eff343752f75faed
Floriane Izard <floriane.izd45@gmail.com>
2022-09-12 22:19:08 +0200

    add = input, delete, check

--- js/app.js
+++ js/app.js
@@ -1,2 +1,53 @@
+//SELECTEURS
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
+//ECOUTEURS
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+//FUNCTIONS
+function addTodo(event) {
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Créer le Li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Bouton Check
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Bouton Supprimer
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //AJOUTER NOTRE TODO À TODO-LIST
+    todoList.appendChild(todoDiv);
+    todoInput.value= ""; //permets une fois le todoInput entrer, qu'il soit vide
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //DELETE TODO
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        });
+    }
+
+        //CHECK MARK
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
 
